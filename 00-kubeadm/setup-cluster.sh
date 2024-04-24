a886a20159f88db8d241325d0c055b6e2f7fe47f
Ravikanth C <ravikanth@ravichaganti.com>
2024-04-24 20:24:47 +0530

    Updated to support kubernetes 1.30

--- 00-kubeadm/setup-cluster.sh
+++ 00-kubeadm/setup-cluster.sh
@@ -4,7 +4,8 @@
 CONTAINERD_VERSION=${CONTAINERD_VERSION:-"1.7.15"}
 RUNC_VERSION=${RUNC_VERSION:-"1.1.12"}
 CNI_PLUGINS_VERSION=${CNI_PLUGINS_VERSION:-"1.4.1"}
-IS_CONTROL_PLANE=${IS_CONTROL_PLANE:-"true"}
+KUBERNETES_VERSION=${KUBERNETES_VERSION:-"1.30"}
+IS_CONTROL_PLANE=${IS_CONTROL_PLANE:-"false"}
 
 # Open the necessary ports
 sudo ufw allow proto tcp from any to any port 6443,2379,2380,10250,10257,10259,179
@@ -73,16 +74,16 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 # If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
 # sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# Create a cluster using kubeadm - Run only on CP
+# Create a cluster using kubeadm - Run only on CP1
 if [ "$IS_CONTROL_PLANE" = true]; then
    NODENAME=$(hostname -s)
    IPADDR=$(hostname -I)
